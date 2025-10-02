285ee081fd9c1cae1ec143f7e6ea8068851c0570
Diego Paladini <diegopalladini@gmail.com>
2025-10-02 17:48:38 -0300

    degub falso

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from projeto import app #da pasta projeto, importe o arquivo __init__
 
 # essa linha abaixo poderia ser apenas "app.run()", sem o if, que funcionaria
 if __name__ == "__main__": #Se eu executar o arquivo main, é pra executar app.run()
-    app.run(debug=True)
+    app.run(debug=False)
